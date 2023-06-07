50c06dc61352030797d3c7e22c6de79652949ea0
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-06-07 16:18:58 -0400

    setting up beachball

---
+++ beachball.config.js
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+/** @type {import("beachball").BeachballConfig } */
+module.exports = {
+    bumpDeps: false,
+    access: "public",
+    tag: "latest",
+    ignorePatterns: [
+      ".nycrc",
+      ".eslintrc.json",
+      ".mocharc.json",
+      "tsconfig.*",
+      ".*ignore",
+      ".github/**",
+      ".vscode/**",
+      "pnpm-lock.yaml",
+    ],
+    changehint: "Run 'pnpm change' to generate a change file",
+  };
