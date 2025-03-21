c58a6cddbb9e488813e927bf8bf73c3ae7e24dc0
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-03-21 16:43:39 +0100

    Update version in __init.py

--- python_sdk/tests/test_version.py
+++ python_sdk/tests/test_version.py
@@ -6,14 +6,14 @@ from importlib import metadata, import_module
 
 # Taken from https://github.com/python/peps/issues/226
 # See https://peps.python.org/pep-0440/
-semver_regex = (r'^([1-9]\d*!)?(0|[1-9]\d*)(\.(0|[1-9]\d*))*((a|b|rc)(0|[1-9]\d*))?'
+pepver_regex = (r'^([1-9]\d*!)?(0|[1-9]\d*)(\.(0|[1-9]\d*))*((a|b|rc)(0|[1-9]\d*))?'
                 r'(\.post(0|[1-9]\d*))?(\.dev(0|[1-9]\d*))?$')
 
 
 class TestVersion:
     def test_version(self):
         from reality_capture import __version__
-        assert re.match(semver_regex, __version__)
+        assert re.match(pepver_regex, __version__)
 
     def test_version_fails(self):
         if "reality_capture" in sys.modules:
@@ -22,4 +22,4 @@ class TestVersion:
         with patch("importlib.metadata.version", side_effect=metadata.PackageNotFoundError):
             reality_capture = import_module("reality_capture")  # Force re-import
             assert reality_capture.__version__ == "0.0.dev0"
-            assert re.match(semver_regex, reality_capture.__version__)
+            assert re.match(pepver_regex, reality_capture.__version__)
