0a40f05e5b192e07c0d3b454ebf4085ccbf43f4c
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-03-24 13:49:41 +0100

    Add action to publish pre releases
    Co-authored-by: Cyril Novel <5690282+cnovel@users.noreply.github.com>

--- python_sdk/src/reality_capture/__init__.py
+++ python_sdk/src/reality_capture/__init__.py
@@ -3,4 +3,4 @@ from importlib import metadata
 try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError as e:
-    __version__ = "0.0.0-clone"
+    __version__ = "0.0.dev0"
