94e2fa25435bf751ce694f1a45ddf2e870aa8dbd
Cyril Novel <5690282+cnovel@users.noreply.github.com>
2025-03-27 14:13:56 +0100

    Missed an enum

--- python_sdk/src/reality_capture/specifications/calibration.py
+++ python_sdk/src/reality_capture/specifications/calibration.py
@@ -17,7 +17,6 @@ class CalibrationOutputs(BaseModel):
 class CalibrationOutputsCreate(Enum):
     SCENE = "scene"
     REPORT = "report"
-    ORIENTATIONS = "orientations"
     SPLATS = "splats"
 
 
