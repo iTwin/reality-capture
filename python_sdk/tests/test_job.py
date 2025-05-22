5afe85d27114a4671373a3ad41c29bc2c4289092
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-05-22 16:59:51 +0200

    Fix test_job

--- python_sdk/tests/test_job.py
+++ python_sdk/tests/test_job.py
@@ -6,7 +6,7 @@ class TestJob:
     def test_appropriate_service_job(self):
         cdt = datetime.datetime(1974, 9, 1, 0, 0, 0)
         cdt = {"createdDateTime": cdt, "startedDateTime": None, "endedDateTime": None, "estimatedUnits": None}
-        ts = {"inputs": {"scene": "scene"}, "outputs": {"reference_model": {"location": "location"}}}
+        ts = {"inputs": {"scene": "scene"}, "outputs": {"referenceModel": {"location": "location"}}}
         j = Job(id="id", type=JobType.TILING, iTwinId="itwin", state=JobState.SUCCESS, executionInfo=cdt,
                 userId="claude@example.org", specifications=ts, bucketId="bucket")
         assert j.get_appropriate_service() == Service.MODELING
@@ -18,7 +18,7 @@ class TestJob:
         assert j.get_appropriate_service() == Service.CONVERSION
 
     def test_appropriate_service_job_create(self):
-        ts = {"inputs": {"scene": "scene"}, "outputs": {"reference_model": {"location": "location"}}}
+        ts = {"inputs": {"scene": "scene"}, "outputs": {"referenceModel": {"location": "location"}}}
         j = JobCreate(**{"type": JobType.TILING, "iTwinId": "itwin", "specifications": ts})
         assert j.get_appropriate_service() == Service.MODELING
         j = JobCreate(**{"type": JobType.EXTRACT_GROUND, "iTwinId": "itwin", "specifications": ts})
