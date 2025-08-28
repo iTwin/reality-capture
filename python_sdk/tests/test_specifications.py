8845f718ff26f185f5cdd71de1d87b1650801c09
Cyril <5690282+cnovel@users.noreply.github.com>
2025-08-28 17:12:55 +0200

    Remove formats (#191)

--- python_sdk/tests/test_specifications.py
+++ python_sdk/tests/test_specifications.py
@@ -1,25 +1,26 @@
-import reality_capture.specifications.production  as production 
+import reality_capture.specifications.production as production
+
 
 class TestSpecifications:
 
     def test_export_options_parsing(self):
-        export_3sm_dict = { "format" : "3SM",
-                            "name" : "Production_5",
-                            "options" : {
-                                "textureColorSource" : "Visible",
-                            }
-                          }
-        export_create_3sm = production.ExportCreate(**export_3sm_dict) 
-        assert isinstance(export_create_3sm.options, production.Options3SM)
+        export_obj_dict = {"format": "OBJ",
+                           "name": "Production_5",
+                           "options": {
+                               "textureColorSource": "Visible",
+                           }
+                           }
+        export_create_obj = production.ExportCreate(**export_obj_dict)
+        assert isinstance(export_create_obj.options, production.OptionsOBJ)
 
-        export_3DTiles_dict = { "format" : "3DTiles",
-                            "name" : "Production_5",
-                            "options" : {
-                                "textureColorSource" : "Visible",
-                            }
-                          }
-        export_create_3DTiles = production.ExportCreate(**export_3DTiles_dict) 
-        assert isinstance(export_create_3DTiles.options, production.Options3DTiles)
+        export_3dtiles_dict = {"format": "3DTiles",
+                               "name": "Production_5",
+                               "options": {
+                                   "textureColorSource": "Visible",
+                               }
+                               }
+        export_create_3dtiles = production.ExportCreate(**export_3dtiles_dict)
+        assert isinstance(export_create_3dtiles.options, production.Options3DTiles)
 
     def test_export_options_from_object(self):
-        construct_from_object = production.ExportCreate(format=production.Format.THREESM, options=production.Options3SM())
+        construct_from_object = production.ExportCreate(format=production.Format.OBJ, options=production.OptionsOBJ())
