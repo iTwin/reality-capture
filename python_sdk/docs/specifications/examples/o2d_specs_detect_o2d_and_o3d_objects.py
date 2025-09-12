7eb599c84aa400147158e459aa20649c76cb5895
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-09-12 10:42:13 +0200

    Comment analysis doc examples

--- python_sdk/docs/specifications/examples/o2d_specs_detect_o2d_and_o3d_objects.py
+++ python_sdk/docs/specifications/examples/o2d_specs_detect_o2d_and_o3d_objects.py
@@ -1,7 +1,7 @@
-import reality_capture.specifications.objects2d as objects2d
-
-o2d_inputs = objects2d.Objects2DInputs(photos="587a14fd-305a-474c-b037-26d4ee8829d9",
-                                       photoObjectDetector="08342927-859c-4563-a4b8-6c6cfb7d5bb3")
-o2d_outputs = [objects2d.Objects2DOutputsCreate.OBJECTS2D, objects2d.Objects2DOutputsCreate.OBJECTS3D]
-o2d_options = objects2d.Objects2DOptions()
-o2ds = objects2d.Objects2DSpecificationsCreate(inputs=o2d_inputs, outputs=o2d_outputs, options=o2d_options)
+# import reality_capture.specifications.objects2d as objects2d
+#
+# o2d_inputs = objects2d.Objects2DInputs(photos="587a14fd-305a-474c-b037-26d4ee8829d9",
+#                                        photoObjectDetector="08342927-859c-4563-a4b8-6c6cfb7d5bb3")
+# o2d_outputs = [objects2d.Objects2DOutputsCreate.OBJECTS2D, objects2d.Objects2DOutputsCreate.OBJECTS3D]
+# o2d_options = objects2d.Objects2DOptions()
+# o2ds = objects2d.Objects2DSpecificationsCreate(inputs=o2d_inputs, outputs=o2d_outputs, options=o2d_options)
--- python_sdk/docs/specifications/examples/s2d_specs_segment_photos_and_project_on_mesh.py
+++ python_sdk/docs/specifications/examples/s2d_specs_segment_photos_and_project_on_mesh.py
@@ -1,11 +1,11 @@
-import reality_capture.specifications.segmentation2d as segmentation2d
-
-s2d_inputs = segmentation2d.Segmentation2DInputs(photos="401975b7-0c0a-4498-5896-84987921f4bb",
-                                                 photoSegmentationDetector="08342927-859c-4563-a4b8-6c6cfb7d5bb3",
-                                                 meshes="c6169435-0c50-7e79-b323-dfff4a499c60")
-s2d_outputs = [segmentation2d.Segmentation2DOutputsCreate.SEGMENTATION2D,
-               segmentation2d.Segmentation2DOutputsCreate.SEGMENTED_PHOTOS,
-               segmentation2d.Segmentation2DOutputsCreate.LINES3D,
-               segmentation2d.Segmentation2DOutputsCreate.POLYGONS3D]
-s2d_options = segmentation2d.Segmentation2DOptions()
-s2ds = segmentation2d.Segmentation2DSpecificationsCreate(inputs=s2d_inputs, outputs=s2d_outputs, options=s2d_options)
+# import reality_capture.specifications.segmentation2d as segmentation2d
+#
+# s2d_inputs = segmentation2d.Segmentation2DInputs(photos="401975b7-0c0a-4498-5896-84987921f4bb",
+#                                                  photoSegmentationDetector="08342927-859c-4563-a4b8-6c6cfb7d5bb3",
+#                                                  meshes="c6169435-0c50-7e79-b323-dfff4a499c60")
+# s2d_outputs = [segmentation2d.Segmentation2DOutputsCreate.SEGMENTATION2D,
+#                segmentation2d.Segmentation2DOutputsCreate.SEGMENTED_PHOTOS,
+#                segmentation2d.Segmentation2DOutputsCreate.LINES3D,
+#                segmentation2d.Segmentation2DOutputsCreate.POLYGONS3D]
+# s2d_options = segmentation2d.Segmentation2DOptions()
+# s2ds = segmentation2d.Segmentation2DSpecificationsCreate(inputs=s2d_inputs, outputs=s2d_outputs, options=s2d_options)
