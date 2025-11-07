c49c82d5ffd9d8b480c654abe945281459e3bca4
Cyril Novel <5690282+cnovel@users.noreply.github.com>
2025-11-07 09:08:19 +0100

    Rename folder from typescript_sdk to typescript

--- python_sdk/tests/test_job.py
+++ python_sdk/tests/test_job.py
@@ -2,7 +2,6 @@ import datetime
 from reality_capture.service.job import Service, JobCreate, Job, JobType, JobState
 from reality_capture.specifications.tiling import TilingOutputsCreate
 # from reality_capture.specifications.segmentation3d import Segmentation3DOutputsCreate
-# from reality_capture.specifications.point_cloud_conversion import PCConversionOutputsCreate
 
 
 class TestJob:
@@ -11,16 +10,16 @@ class TestJob:
         cdt = {"createdDateTime": cdt, "startedDateTime": None, "endedDateTime": None, "estimatedUnits": None}
         tiling_specs = {"inputs": {"scene": "scene"}, "outputs": {"modelingReference": {"location": "location"}}}
         j = Job(id="id", type=JobType.TILING, iTwinId="itwin", state=JobState.SUCCESS, executionInfo=cdt,
-                userId="claude@example.org", specifications=tiling_specs, bucketId="bucket")
+                userId="claude@example.org", specifications=tiling_specs)
         assert j.get_appropriate_service() == Service.MODELING
         # training_specs = {"inputs": {"scene": "scene"}, "outputs": {"detector": "detector"}}
         # j = Job(id="id", type=JobType.TRAINING_O2D, iTwinId="itwin", state=JobState.SUCCESS, executionInfo=cdt,
         #         userId="claude@example.org", specifications=training_specs, bucketId="bucket")
         # assert j.get_appropriate_service() == Service.ANALYSIS
-        # pc_conversion_specs = {"inputs": {"pointClouds": ["point_cloud"]}, "outputs": {"opc": "opc"}}
-        # j = Job(id="id", type=JobType.POINT_CLOUD_CONVERSION, iTwinId="itwin", state=JobState.SUCCESS,
-        #         executionInfo=cdt, userId="claude@example.org", specifications=pc_conversion_specs, bucketId="bucket")
-        # assert j.get_appropriate_service() == Service.CONVERSION
+        pc_conversion_specs = {"inputs": {"pointCloud": "point_cloud"}, "outputs": "youpi"}
+        j = Job(id="id", type=JobType.POINT_CLOUD_CONVERSION, iTwinId="itwin", state=JobState.SUCCESS,
+                executionInfo=cdt, userId="claude@example.org", specifications=pc_conversion_specs)
+        assert j.get_appropriate_service() == Service.CONVERSION
 
     def test_appropriate_service_job_create(self):
         tiling_specs = {"inputs": {"scene": "scene"}, "outputs": [TilingOutputsCreate.MODELING_REFERENCE]}
@@ -31,6 +30,6 @@ class TestJob:
         #                         Segmentation3DOutputsCreate.SEGMENTED_POINT_CLOUD]}
         # j = JobCreate(**{"type": JobType.SEGMENTATION_3D, "iTwinId": "itwin", "specifications": eg_specs})
         # assert j.get_appropriate_service() == Service.ANALYSIS
-        # pc_conversion_specs = {"inputs": {"pointClouds": ["point_cloud"]}, "outputs": PCConversionOutputsCreate.OPC}
-        # j = JobCreate(**{"type": JobType.POINT_CLOUD_CONVERSION, "iTwinId": "itwin", "specifications": pc_conversion_specs})
-        # assert j.get_appropriate_service() == Service.CONVERSION
+        pc_conversion_specs = {"inputs": {"pointCloud": "point_cloud"}}
+        j = JobCreate(**{"type": JobType.POINT_CLOUD_CONVERSION, "iTwinId": "itwin", "specifications": pc_conversion_specs})
+        assert j.get_appropriate_service() == Service.CONVERSION
