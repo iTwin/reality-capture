1b1620954aa268dd14babf9052eb4a43f0a84c37
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-11-27 13:24:34 +0100

    Fix ts files indent (2 spaces instead of 4) (#244)

--- typescript/examples/src/example_upload_reality_data.ts
+++ typescript/examples/src/example_upload_reality_data.ts
@@ -11,65 +11,65 @@ import { RealityDataClientOptions, RealityDataAccessClient, ITwinRealityData } f
 export async function sleep(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 async function runUploadExample() {
-    /**
-     * This example shows how to upload and download data from/to an iTwin
-     */
+  /**
+   * This example shows how to upload and download data from/to an iTwin
+   */
 
-    // Inputs to provide. Please, adapt values
-    const imagesPath = "D:/Datasets/Helico/Images";
-    const outputPath = "D:/Downloads/Helico/Images";
+  // Inputs to provide. Please, adapt values
+  const imagesPath = "D:/Datasets/Helico/Images";
+  const outputPath = "D:/Downloads/Helico/Images";
 
-    const imagesName = "Reality Capture SDK context scene example";
+  const imagesName = "Reality Capture SDK context scene example";
 
-    // Script
-    dotenv.config();
+  // Script
+  dotenv.config();
 
-    const iTwinId = process.env.IMJS_ITWIN_ID ?? "";
-    const clientId = process.env.IMJS_CLIENT_ID ?? "";
-    const secret = process.env.IMJS_CLIENT_SECRET ?? "";
-    const issuerUrl = "https://ims.bentley.com";
+  const iTwinId = process.env.IMJS_ITWIN_ID ?? "";
+  const clientId = process.env.IMJS_CLIENT_ID ?? "";
+  const secret = process.env.IMJS_CLIENT_SECRET ?? "";
+  const issuerUrl = "https://ims.bentley.com";
 
-    const authorizationClient = new ServiceAuthorizationClient({
-        clientId: clientId,
-        scope: "itwin-platform",
-        clientSecret: secret,
-        authority: issuerUrl,
-    });
+  const authorizationClient = new ServiceAuthorizationClient({
+    clientId: clientId,
+    scope: "itwin-platform",
+    clientSecret: secret,
+    authority: issuerUrl,
+  });
 
-    const realityDataHandler = new RealityDataHandler(authorizationClient);
-    console.log("Reality Data handler initialized");
+  const realityDataHandler = new RealityDataHandler(authorizationClient);
+  console.log("Reality Data handler initialized");
 
-    const realityDataClientOptions: RealityDataClientOptions = {
-      authorizationClient: authorizationClient,
-      baseUrl: "https://api.bentley.com/reality-management/reality-data",
-    };
-    const realityDataClient = new RealityDataAccessClient(realityDataClientOptions);
-    console.log("Reality Data Client initialized");
+  const realityDataClientOptions: RealityDataClientOptions = {
+    authorizationClient: authorizationClient,
+    baseUrl: "https://api.bentley.com/reality-management/reality-data",
+  };
+  const realityDataClient = new RealityDataAccessClient(realityDataClientOptions);
+  console.log("Reality Data Client initialized");
 
-    try {
-        console.log("Upload images in ", iTwinId);
-        const realityData = new ITwinRealityData(realityDataClient, undefined, iTwinId);
-        realityData.displayName = imagesName;
-        realityData.type = "CCImageCollection";
-        const createdRealityData = await realityDataClient.createRealityData("", iTwinId, realityData);
-        const response = await realityDataHandler.uploadData(createdRealityData.id, imagesPath, "", iTwinId);
-        if(response.isError()) {
-            console.log("Failed to upload reality data : " + response.error!.error.message);
-            return;
-        }
-        console.log("Successfully uploaded images")
-
-        console.log("Downloading images in ", outputPath);
-        const responseDownload = await realityDataHandler.downloadData(createdRealityData.id, outputPath, "", iTwinId);
-        if(responseDownload.isError()) {
-            console.log("Failed to download reality data : " + responseDownload.error!.error.message);
-            return;
-        }
-        console.log("Successfully downloaded images");
+  try {
+    console.log("Upload images in ", iTwinId);
+    const realityData = new ITwinRealityData(realityDataClient, undefined, iTwinId);
+    realityData.displayName = imagesName;
+    realityData.type = "CCImageCollection";
+    const createdRealityData = await realityDataClient.createRealityData("", iTwinId, realityData);
+    const response = await realityDataHandler.uploadData(createdRealityData.id, imagesPath, "", iTwinId);
+    if (response.isError()) {
+      console.log("Failed to upload reality data : " + response.error!.error.message);
+      return;
     }
-    catch(error: any) {
-        console.log(error);
+    console.log("Successfully uploaded images")
+
+    console.log("Downloading images in ", outputPath);
+    const responseDownload = await realityDataHandler.downloadData(createdRealityData.id, outputPath, "", iTwinId);
+    if (responseDownload.isError()) {
+      console.log("Failed to download reality data : " + responseDownload.error!.error.message);
+      return;
     }
+    console.log("Successfully downloaded images");
+  }
+  catch (error: any) {
+    console.log(error);
+  }
 }
 
 runUploadExample();
