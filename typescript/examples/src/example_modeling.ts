0283356f5a9649596279c23bae7869bf1f8a088e
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2025-12-01 09:53:25 +0100

    Fix typescript examples (#245)

--- typescript/examples/src/example_modeling.ts
+++ typescript/examples/src/example_modeling.ts
@@ -11,12 +11,117 @@ import {
   FillImagePropertiesOutputsCreate, FillImagePropertiesOptions, FillImagePropertiesOutputs,
   RealityCaptureService, ReconstructionSpecificationsCreate, ReconstructionInputs, ReconstructionOutputsCreate,
   ReconstructionOutputs, ExportCreate, Format, OptionsLAS, SamplingStrategy, TilingOptions, GeometricPrecision,
-  JobCreate, JobType, JobState, Progress, getAppropriateService, RealityDataHandler
+  JobCreate, JobType, JobState, Progress, getAppropriateService, RealityDataHandler,
+  Options3DTiles, BucketDataHandler, AdjustmentConstraints,
+  LODScope
 } from "@itwin/reality-capture";
 import { RealityDataClientOptions, RealityDataAccessClient, ITwinRealityData } from "@itwin/reality-data-client";
+import path from "path";
 
 
 export async function sleep(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+async function monitorJob(realityCaptureService: RealityCaptureService, jobId: string) {
+  // Monitor job progress every 10s
+  let progress: Progress = { state: JobState.QUEUED, percentage: 0 };
+  while (progress.state !== JobState.CANCELLED && progress.state !== JobState.FAILED && progress.state !== JobState.SUCCESS) {
+    const response = await realityCaptureService.getJobProgress(jobId, getAppropriateService(JobType.RECONSTRUCTION));
+    if (response.isError()) {
+      console.log("Can't get job progress");
+    }
+    progress = response.value!;
+    console.log("Progress : ", progress.percentage);
+    await sleep(10000);
+    if (progress.state === JobState.CANCELLED || progress.state === JobState.FAILED) {
+      console.log("Job not completed");
+    }
+    else if (progress.state === JobState.SUCCESS) {
+      console.log("Job completed");
+    }
+  }
+}
+
+async function runFillImageProperties(realityCaptureService: RealityCaptureService, imagesRealityData: ITwinRealityData, fipJobName: string, iTwinId: string): Promise<string> {
+  // Submit FillImageProperties job to get a context scene from the images
+  let fipInputs: FillImagePropertiesInputs = { imageCollections: [imagesRealityData.id] };
+  let fipOutputs = [FillImagePropertiesOutputsCreate.SCENE];
+  let fipOptions: FillImagePropertiesOptions = {};
+  let fipSpecs: FillImagePropertiesSpecificationsCreate = { inputs: fipInputs, outputs: fipOutputs, options: fipOptions };
+  let fipJobToSubmit: JobCreate = { name: fipJobName, specifications: fipSpecs, type: JobType.FILL_IMAGE_PROPERTIES, iTwinId: iTwinId };
+  const submitFipResponse = await realityCaptureService.submitJob(fipJobToSubmit);
+  if (submitFipResponse.isError()) {
+    throw new Error("Failed to submit FillImageProperties job : " + submitFipResponse.error!.error.message);
+  }
+  const fipJobId = submitFipResponse.value!.id;
+  console.log("FillImageProperties job submitted");
+  await monitorJob(realityCaptureService, fipJobId);
+  const fipPropertiesResponse = await realityCaptureService.getJob(fipJobId, getAppropriateService(JobType.FILL_IMAGE_PROPERTIES));
+  if (fipPropertiesResponse.isError()) {
+    throw new Error("Failed to retrieve FillImageProperties job properties : " + fipPropertiesResponse.error!.error.message);
+  }
+  return (fipPropertiesResponse.value!.specifications.outputs as FillImagePropertiesOutputs).scene;
+}
+
+async function runCalibration(realityCaptureService: RealityCaptureService, bucketDataHandler: BucketDataHandler, fipOutputContextScene: string, calibJobName: string, outputPath: string, iTwinId: string, calibOptions: CalibrationOptions): Promise<string> {
+  // Submit Calibration job to get an oriented context scene
+  let calibInputs: CalibrationInputs = { scene: fipOutputContextScene };
+  let calibOutputs = [CalibrationOutputsCreate.SCENE, CalibrationOutputsCreate.REPORT];
+  let calibSpecs: CalibrationSpecificationsCreate = { inputs: calibInputs, outputs: calibOutputs, options: calibOptions };
+  let calibJobToSubmit: JobCreate = { name: calibJobName, specifications: calibSpecs, type: JobType.CALIBRATION, iTwinId: iTwinId };
+  const submitCalibResponse = await realityCaptureService.submitJob(calibJobToSubmit);
+  if (submitCalibResponse.isError()) {
+    throw new Error("Failed to submit Calibration job : " + submitCalibResponse.error!.error.message);
+  }
+  const calibJobId = submitCalibResponse.value!.id;
+  console.log("Calibration job submitted");
+  await monitorJob(realityCaptureService, calibJobId);
+  const calibPropertiesResponse = await realityCaptureService.getJob(calibJobId, getAppropriateService(JobType.CALIBRATION));
+  if (calibPropertiesResponse.isError()) {
+    throw new Error("Failed to retrieve Calibration job properties : " + calibPropertiesResponse.error!.error.message);
+  }
+  const outputs = calibPropertiesResponse.value!.specifications.outputs as CalibrationOutputs;
+  const chars = outputs.report!.split(":"); // Remove 'bkt:' from the report bucket path
+  const calibDownloadResponse = await bucketDataHandler.downloadData(iTwinId, path.join(outputPath, "Report"), chars[1]);
+  if (calibDownloadResponse.isError()) {
+    throw new Error("Failed to download report " + calibDownloadResponse.error!.error.message);
+  }
+  return (calibPropertiesResponse.value!.specifications.outputs as CalibrationOutputs).scene;
+}
+
+async function runReconstruction(realityCaptureService: RealityCaptureService, realityDataHandler: RealityDataHandler, calibOutputContextScene: string, reconsJobName: string, outputPath: string, iTwinId: string, 
+  lasOptions: OptionsLAS, tiles3dOptions: Options3DTiles) {
+  // Submit Reconstruction job to generate the LAS
+  let reconsInputs: ReconstructionInputs = { scene: calibOutputContextScene };
+  let exportLas: ExportCreate = { format: Format.LAS, options: lasOptions };
+  let export3dTiles: ExportCreate = { format: Format.THREED_TILES, options: tiles3dOptions };
+  let reconsOutputs: ReconstructionOutputsCreate = { exports: [exportLas, export3dTiles] };
+  let tilingOptions: TilingOptions = { geometricPrecision: GeometricPrecision.EXTRA };
+  let reconsSpecs: ReconstructionSpecificationsCreate = { inputs: reconsInputs, outputs: reconsOutputs, options: tilingOptions };
+  let reconsJobToSubmit: JobCreate = { name: reconsJobName, specifications: reconsSpecs, type: JobType.RECONSTRUCTION, iTwinId: iTwinId };
+  const submitReconsResponse = await realityCaptureService.submitJob(reconsJobToSubmit);
+  if (submitReconsResponse.isError()) {
+    throw new Error("Failed to submit Reconstruction job : " + submitReconsResponse.error!.error.message);
+  }
+  const reconsJobId = submitReconsResponse.value!.id;
+  console.log("Reconstruction job submitted");
+  await monitorJob(realityCaptureService, reconsJobId);
+
+  console.log("Downloading Reconstruction LAS output");
+  const reconsPropertiesResponse = await realityCaptureService.getJob(reconsJobId, getAppropriateService(JobType.RECONSTRUCTION));
+  if (reconsPropertiesResponse.isError()) {
+    throw new Error("Failed to retrieve Reconstruction job properties : " + reconsPropertiesResponse.error!.error.message);
+  }
+  const outputs = reconsPropertiesResponse.value!.specifications.outputs as ReconstructionOutputs;
+  for(let reconsExport of outputs.exports!)
+  {
+    const reconsDownloadResponse = await realityDataHandler.downloadData(reconsExport.location, path.join(outputPath, reconsExport.format), "", iTwinId);
+    if (reconsDownloadResponse.isError()) {
+      throw new Error("Failed to download export " +  reconsExport.format +  " : " + reconsDownloadResponse.error!.error.message);
+    }
+  }
+  console.log("Successfully downloaded Reconstruction LAS output");
+}
+
 
 async function runModelingExample() {
   /**
@@ -25,12 +130,23 @@ async function runModelingExample() {
 
   // Inputs to provide. Please, adapt values
   const imagesPath = "D:/Datasets/Helico/Images";
-  const outputPath = "D:/Datasets/Helico/LAS";
+  const outputPath = "D:/Datasets/Helico/Results";
 
-  // Optional : sampling distance (in meter). Please, set to undefined if you don't want to specify sampling distance
-  const samplingDistance: number | undefined = 0.5;
-  // Optional : srs used in outputs. Please, set to undefined if you don't want to specify srs
-  const crs: string | undefined = "EPSG:32631";
+  // Options for calibration
+  const calibOptions: CalibrationOptions = { 
+    adjustmentConstraints: [AdjustmentConstraints.POSITION_METADATA] // Adjustment from photos position metadata
+  };
+
+  // Options for Reconstruction LAS export
+  const lasOptions: OptionsLAS = {
+    samplingDistance: 0.5, // sampling distance (in meter).
+    samplingStrategy: SamplingStrategy.ABSOLUTE,
+    crs: "EPSG:32631" // CRS used for this export
+  }
+  // Options for Reconstruction 3DTiles export
+  const tiles3dOptions: Options3DTiles = {
+    lodScope: LODScope.ACROSS_TILES // produce global LOD structure (e.g., a quadtree) for an entire reconstruction
+  }
 
   // Optional : images reality data name
   const imagesRealityDataName = "Reality Capture SDK sample images"
@@ -60,32 +176,15 @@ async function runModelingExample() {
   const realityDataHandler = new RealityDataHandler(authorizationClient);
   console.log("Reality Data handler initialized");
 
+  const bucketDataHandler = new BucketDataHandler(authorizationClient);
+  console.log("Bucket Data handler initialized");
+
   const realityDataClientOptions: RealityDataClientOptions = {
     authorizationClient: authorizationClient,
     baseUrl: "https://api.bentley.com/reality-management/reality-data",
   };
   const realityDataClient = new RealityDataAccessClient(realityDataClientOptions);
   console.log("Reality Data Client initialized");
-
-  // Function to monitor job progress
-  let monitorJob = async (jobId: string) => {
-    let progress: Progress = { state: JobState.QUEUED, percentage: 0 };
-    while (progress.state !== JobState.CANCELLED && progress.state !== JobState.FAILED && progress.state !== JobState.SUCCESS) {
-      const response = await realityCaptureService.getJobProgress(jobId, getAppropriateService(JobType.RECONSTRUCTION));
-      if (response.isError()) {
-        console.log("Can't get job progress");
-      }
-      progress = response.value!;
-      console.log("progress : ", progress.percentage);
-      await sleep(10000);
-      if (progress.state === JobState.CANCELLED || progress.state === JobState.FAILED) {
-        console.log("Job not completed");
-      }
-      else if (progress.state === JobState.SUCCESS) {
-        console.log("Job completed");
-      }
-    }
-  };
 
   try {
     console.log("Upload images in ", iTwinId);
@@ -95,89 +194,15 @@ async function runModelingExample() {
     const createdRealityData = await realityDataClient.createRealityData("", iTwinId, realityData);
     const uploadResponse = await realityDataHandler.uploadData(createdRealityData.id, imagesPath, "", iTwinId);
     if (uploadResponse.isError()) {
-      console.log("Failed to upload reality data : " + uploadResponse.error!.error.message);
-      return;
+      throw new Error("Failed to upload reality data : " + uploadResponse.error!.error.message);
     }
     console.log("Successfully uploaded images");
 
-    // Submit FillImageProperties job to get a context scene from the images
-    let fipInputs: FillImagePropertiesInputs = { imageCollections: [createdRealityData.id] };
-    let fipOutputs = [FillImagePropertiesOutputsCreate.SCENE];
-    let fipOptions: FillImagePropertiesOptions = {};
-    let fipSpecs: FillImagePropertiesSpecificationsCreate = { inputs: fipInputs, outputs: fipOutputs, options: fipOptions };
-    let fipJobToSubmit: JobCreate = { name: fipJobName, specifications: fipSpecs, type: JobType.FILL_IMAGE_PROPERTIES, iTwinId: iTwinId };
-    const submitFipResponse = await realityCaptureService.submitJob(fipJobToSubmit);
-    if (submitFipResponse.isError()) {
-      console.log("Failed to submit FillImageProperties job : " + submitFipResponse.error!.error.message);
-      return;
-    }
-    const fipJobId = submitFipResponse.value!.id;
-    console.log("FillImageProperties job submitted");
-    await monitorJob(fipJobId);
-    const fipPropertiesResponse = await realityCaptureService.getJob(fipJobId, getAppropriateService(JobType.FILL_IMAGE_PROPERTIES));
-    if (fipPropertiesResponse.isError()) {
-      console.log("Failed to retrieve FillImageProperties job properties : " + submitFipResponse.error!.error.message);
-      return;
-    }
-    const fipOutputContextScene = (fipPropertiesResponse.value!.specifications.outputs as FillImagePropertiesOutputs).scene;
+    const fipOutputContextScene = await runFillImageProperties(realityCaptureService, createdRealityData, fipJobName, iTwinId);
 
-    // Submit Calibration job to get an oriented context scene
-    let calibInputs: CalibrationInputs = { scene: fipOutputContextScene };
-    let calibOutputs = [CalibrationOutputsCreate.SCENE];
-    let calibOptions: CalibrationOptions = {};
-    let calibSpecs: CalibrationSpecificationsCreate = { inputs: calibInputs, outputs: calibOutputs, options: calibOptions };
-    let calibJobToSubmit: JobCreate = { name: calibJobName, specifications: calibSpecs, type: JobType.CALIBRATION, iTwinId: iTwinId };
-    const submitCalibResponse = await realityCaptureService.submitJob(calibJobToSubmit);
-    if (submitCalibResponse.isError()) {
-      console.log("Failed to submit Calibration job : " + submitCalibResponse.error!.error.message);
-      return;
-    }
-    const calibJobId = submitCalibResponse.value!.id;
-    console.log("Calibration job submitted");
-    await monitorJob(calibJobId);
-    const calibPropertiesResponse = await realityCaptureService.getJob(calibJobId, getAppropriateService(JobType.CALIBRATION));
-    if (calibPropertiesResponse.isError()) {
-      console.log("Failed to retrieve Calibration job properties : " + submitCalibResponse.error!.error.message);
-      return;
-    }
-    const calibOutputContextScene = (calibPropertiesResponse.value!.specifications.outputs as CalibrationOutputs).scene;
+    const calibOutputContextScene = await runCalibration(realityCaptureService, bucketDataHandler, fipOutputContextScene, calibJobName, outputPath, iTwinId, calibOptions);
 
-    // Submit Reconstruction job to generate the LAS
-    let reconsInputs: ReconstructionInputs = { scene: calibOutputContextScene };
-    let lasOptions: OptionsLAS = {};
-    if (crs) {
-      lasOptions.crs = crs;
-    }
-    if (samplingDistance) {
-      lasOptions.samplingStrategy = SamplingStrategy.ABSOLUTE;
-      lasOptions.samplingDistance = samplingDistance;
-    }
-    let exp: ExportCreate = { format: Format.LAS, options: lasOptions };
-    let tilingOptions: TilingOptions = { geometricPrecision: GeometricPrecision.EXTRA };
-    let reconsOutputs: ReconstructionOutputsCreate = { exports: [exp] };
-    let reconsSpecs: ReconstructionSpecificationsCreate = { inputs: reconsInputs, outputs: reconsOutputs, options: tilingOptions };
-    let reconsJobToSubmit: JobCreate = { name: reconsJobName, specifications: reconsSpecs, type: JobType.RECONSTRUCTION, iTwinId: iTwinId };
-    const submitReconsResponse = await realityCaptureService.submitJob(reconsJobToSubmit);
-    if (submitReconsResponse.isError()) {
-      console.log("Failed to submit Reconstruction job : " + submitReconsResponse.error!.error.message);
-      return;
-    }
-    const reconsJobId = submitReconsResponse.value!.id;
-    console.log("Reconstruction job submitted");
-    await monitorJob(reconsJobId);
-
-    console.log("Downloading Reconstruction LAS output");
-    const reconsPropertiesResponse = await realityCaptureService.getJob(reconsJobId, getAppropriateService(JobType.RECONSTRUCTION));
-    if (reconsPropertiesResponse.isError()) {
-      console.log("Failed to retrieve Reconstruction job properties : " + submitReconsResponse.error!.error.message);
-      return;
-    }
-    const reconsDownloadResponse = await realityDataHandler.downloadData((reconsPropertiesResponse.value!.specifications.outputs as ReconstructionOutputs).exports![0].location, outputPath, "", iTwinId);
-    if (reconsDownloadResponse.isError()) {
-      console.log("Failed to download Reconstruction LAS output : " + submitReconsResponse.error!.error.message);
-      return;
-    }
-    console.log("Successfully downloaded Reconstruction LAS output");
+    await runReconstruction(realityCaptureService, realityDataHandler, calibOutputContextScene, reconsJobName, outputPath, iTwinId, lasOptions, tiles3dOptions);
   }
   catch (error: any) {
     console.log(error);
