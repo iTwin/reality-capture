9fbb701dc7b379c658e968ee0e85829cf9fe173a
Liliam Jean Baptiste <liliam.jeanbaptiste@bentley.com>
2026-02-18 19:56:04 +0100

    Updated TrainingS3D example

--- python_sdk/docs/specifications/examples/training_s3d_new_default.py
+++ python_sdk/docs/specifications/examples/training_s3d_new_default.py
@@ -1,12 +1,15 @@
 import reality_capture.specifications.training as training
 
 s3d_inputs = training.TrainingS3DInputs(
-    scene="401975b7-0c0a-4498-5896-84987921f4bb",
+    segmentations3D=["401975b7-0c0a-4498-5896-84987921f4bb"],
+    detectorName="example-detector",
 )
+
 s3d_outputs = [
     training.TrainingS3DOutputsCreate.DETECTOR,
 ]
-s3d_options = training.TrainingS3DOptions()
+s3d_options = training.TrainingS3DOptions(epochs=2, spacing=0.2)
+
 s3ds = training.TrainingS3DSpecificationsCreate(
     inputs=s3d_inputs, outputs=s3d_outputs, options=s3d_options
 )
